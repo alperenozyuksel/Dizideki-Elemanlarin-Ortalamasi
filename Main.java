c712d1da8c4ca51391cfdfe1d0a31be549169cd0
Alperen Ozyuksel <126724611+alperenozyuksel@users.noreply.github.com>
2023-03-16 17:13:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] list = {1.0,2.0,3.0,4.0,5.0};
+
+        double sum = 0.0;
+
+        for (int i = 0; i<list.length; i++){
+
+            sum+=(1/list[i]);
+
+        }
+
+        System.out.print(list.length/sum);
+
+
+
+    }
+}
